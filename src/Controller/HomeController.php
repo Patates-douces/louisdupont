ee69447466c5a1169cc646f981d0e48b1d01ac5b
gregoirelieveaux <gregoire.lieveaux@talkspirit.com>
2020-02-18 11:25:11 +0100

    Controller for home page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Rest\Controller(prefix="/", service="app.home.controller")
+ */
+class HomeController extends AbstractController
+{
+
+    /**
+     * @Route("/" , name = "home")
+     * @return Response
+     */
+    public function index() : Response
+    {
+        return new Response( $this->render('page/home.html.twig'));
+    }
+}
